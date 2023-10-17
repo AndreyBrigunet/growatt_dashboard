e61226e814eba3f6096e31c1463c02147f9408dd
Andrei Brigunet <andreybrigunet@gmail.com>
2023-10-17 10:39:33 +0300

    test

--- collector.py
+++ collector.py
@@ -16,7 +16,7 @@ from dateutil import relativedelta
 from apscheduler.schedulers.background import BlockingScheduler
 
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("APP")
 
 DATABASE_NAME = "data/solar_data.sqlite"
 scheduler = BlockingScheduler()
@@ -26,6 +26,9 @@ SCHEDULER_INTERVAL = 12
 # .open /home/andreybrigunetofficial/growatt_dashboard/data/solar_data.sqlite
 # SELECT * FROM meter;
 
+# json_object = json.dumps(data, indent=4) 
+# with open("sample.json", "w") as outfile:
+#     outfile.write(json_object)
 
 # Based on https://github.com/Antonji-py/solar-providers-manager
 class GrowattApi:
@@ -45,6 +48,7 @@ class GrowattApi:
             "login": "https://server.growatt.com/login",
             "list_plants": "https://server.growatt.com/selectPlant/getPlantList",
             "plant_devices": "https://server.growatt.com/panel/getDevicesByPlantList",
+            "plant_data": "https://server.growatt.com/panel/getPlantData",
             "get_daily_logs_tlx": "https://server.growatt.com/device/getTLXHistory",
             "get_daily_logs_inv": "https://server.growatt.com/device/getInverterHistor",
             "get_monthly_energy": "https://server.growatt.com/energy/compare/getDevicesMonthChart",
@@ -129,7 +133,27 @@ class GrowattApi:
                 return response_json
             pages = response_json["obj"]["pages"]
             devices.extend([device for device in response_json["obj"]["datas"]])
+
+        json_object = json.dumps(devices, indent=4) 
+        with open(f"{plant_id}-devices.json", "w") as outfile:
+            outfile.write(json_object)
         return devices
+
+    def get_plant_data(self, plant_id: str) -> List[Dict]:
+
+        response = self.session.post(
+            self.__fetch_url("plant_data") + f"?plantId={plant_id}"
+        )
+
+        response_json = response.json()
+        if response_json["result"] != 1:
+            return response_json
+        data = response_json["obj"]
+
+        json_object = json.dumps(data, indent=4) 
+        with open(f"{plant_id}.json", "w") as outfile:
+            outfile.write(json_object)
+        return data
 
     def get_daily_logs(
         self, device_id: str, date: str, device_type="tlx"
@@ -195,15 +219,17 @@ class GrowattApi:
                     "start": start_index,
                 },
             )
+
             response_json = response.json()
 
             for log in response_json["obj"]["datas"]:
                 logs.append(log)
 
-            start_index = response_json["obj"]["start"]
             have_next = response_json["obj"]["haveNext"]
 
             if have_next:
+                start_index = response_json["obj"]["start"]
+                date_end = response_json["obj"]["endDate"]
                 time.sleep(10)
 
         logs.reverse()
@@ -235,6 +261,7 @@ class GrowattApi:
 
             if have_next:
                 start_index = response_json["obj"]["start"]
+                date_end = response_json["obj"]["endDate"]
                 time.sleep(5)
 
         logs.reverse()
@@ -278,82 +305,21 @@ class Job:
         if not conf.get("plant_id"):
             Exception("plant_id missing in conf")
 
-    def diff_month(self, current, previous):
-        return (current.year - previous.year) * 12 + current.month - previous.month
-
     def backfill_data(self):
-        self.__create_table()  # first create table before starting operations
-        start_date = datetime.datetime.strptime(
-            self.conf.get("start_date"), "%Y-%m-%d"
-        ).date()
-        days = (datetime.datetime.now().date() - start_date).days + 1
-        months = self.diff_month(datetime.datetime.now().date(), start_date) + 1
+        self.__create_table()
 
-        logger.info(f"Backfilling Pac for {days} days")
-        for count in range(days):
-            self._insert(
-                self.get_time_series_data_pac(
-                    start_date + datetime.timedelta(days=count)
-                ),
-                table_name=self.PAC,
-            )
+        for plant in self.api.get_plants():
+            plant_data = self.api.get_plant_data(plant['id'])
+                
+            start_date = datetime.datetime.strptime(
+                plant_data['creatDate'], "%Y-%m-%d"
+            ).date()
+            days = (datetime.datetime.now().date() - start_date).days + 1
 
-        logger.info(f"Backfilling Kwh for {months} months")
-        for count in range(months):
-            self._insert(
-                self.get_time_series_data_kwh(
-                    start_date + relativedelta.relativedelta(months=count)
-                ),
-                table_name=self.KWH,
-            )
-
-    # def get_time_series_data_pac(self, date: datetime.date) -> List[Tuple]:
-    #     data = self.api.get_daily_energy_data(
-    #         self.conf.get("plant_id"),
-    #         f"{date.year}-{date.month}-{date.day}",
-    #     )
-    #     date = datetime.datetime.combine(date, datetime.datetime.min.time())
-    #     return list(
-    #         zip(
-    #             # converting to string type to enter DB
-    #             # 5 minutes, 288 times ~24 hours
-    #             [
-    #                 (date + datetime.timedelta(minutes=5 * count)).timestamp()
-    #                 for count in range(288)
-    #             ],
-    #             list(
-    #                 map(
-    #                     lambda x: 0 if not x else x,
-    #                     data.get("obj")[0]["datas"]["pac"],
-    #                 )
-    #             ),
-    #         )
-    #     )
-
-    # def get_time_series_data_kwh(self, date: datetime.date) -> List[Tuple]:
-    #     data = self.api.get_monthly_energy_data(
-    #         self.conf.get("plant_id"),
-    #         f"{date.year}-{date.month}",
-    #     )
-    #     date = datetime.datetime.combine(date, datetime.datetime.min.time()).replace(
-    #         day=1
-    #     )
-
-    #     return list(
-    #         zip(
-    #             # daily data
-    #             [
-    #                 (date + datetime.timedelta(days=count)).timestamp()
-    #                 for count in range(calendar.monthrange(date.year, date.month)[1])
-    #             ],
-    #             list(
-    #                 map(
-    #                     lambda x: 0 if not x else x,
-    #                     data.get("obj")[0]["datas"]["energy"],
-    #                 )
-    #             ),
-    #         )
-    #     )
+            logger.info(f"Backfilling for {days} days")
+            for count in range(days):
+                day = start_date + datetime.timedelta(days=count)
+                self.get_history(day, day)
 
     def get_devices(self) -> List[Tuple]:
         plants, meters = [], []
@@ -362,7 +328,6 @@ class Job:
             plants.extend(self.api.get_plant_devices(plant['id']))
             time.sleep(5)
             meters.extend(self.api.get_meters(plant['id']))
-
         return plants, meters
         
     def get_history(
@@ -370,56 +335,57 @@ class Job:
     ) -> List[Tuple]:
         plants, meters = self.get_devices()
 
-        self._insert_plant(
-            self.plant_history(plants, date_start, date_end), 
-            table_name=self.PLANT
-        )
+        for plant in plants:
+            self._insert_plant(
+                self.plant_history(plant, date_start, date_end), 
+                table_name=self.PLANT,
+                plant_id=plant['plantId']
+            )
+            time.sleep(10)
         logger.info(f"Extract plant data from {date_start} to {date_end}")
         
-        self._insert_meter(
-            self.meter_history(meters, date_start, date_end), 
-            table_name=self.METER
-        )
+        for meter in meters:
+            self._insert_meter(
+                self.meter_history(meter, date_start, date_end), 
+                table_name=self.METER
+            )
+            time.sleep(10)
         logger.info(f"Extract meter data from {date_start} to {date_end}")   
 
     def plant_history(
-        self, plants: list, date_start: datetime.date, date_end: datetime.date
+        self, plant: list, date_start: datetime.date, date_end: datetime.date
     ):
         data = []
-        for plant in plants:
-            data.extend(self.api.get_plant_history_data(
-                plant['sn'], 
-                plant['deviceTypeName'],
-                plant['plantId'],
-                f"{date_start.year}-{date_start.month}-{date_start.day}",
-                f"{date_end.year}-{date_end.month}-{date_end.day}",
-            ))
-            time.sleep(10)
-            
+        data.extend(self.api.get_plant_history_data(
+            plant['sn'], 
+            plant['deviceTypeName'],
+            plant['plantId'],
+            date_start.strftime("%Y-%m-%d"),
+            date_end.strftime("%Y-%m-%d"),
+        ))
+        
         return list((
-            self.calendar_to_timestamp(item["calendar"]),
+            self.calendar_to_timestamp(item),
             item["plant_id"],
             item["pac"],
             item["eacToday"],
             item["eacTotal"]) for item in data
         )
-        
+
     def meter_history(
-        self, meters: list, date_start: datetime.date, date_end: datetime.date
+        self, meter: list, date_start: datetime.date, date_end: datetime.date
     ):
         data = []
-        for meter in meters:
-            data.extend(self.api.get_meter_history_data(
-                meter["datalogSn"],
-                meter["deviceType"],
-                meter["addr"],
-                f"{date_start.year}-{date_start.month}-{date_start.day}",
-                f"{date_end.year}-{date_end.month}-{date_end.day}",
-            ))
-            time.sleep(10)
+        data.extend(self.api.get_meter_history_data(
+            meter["datalogSn"],
+            meter["deviceType"],
+            meter["addr"],
+            date_start.strftime("%Y-%m-%d"),
+            date_end.strftime("%Y-%m-%d"),
+        ))
 
         return list((
-            self.calendar_to_timestamp(item["calendar"]),
+            self.calendar_to_timestamp(item),
             item["dataLogSn"], 
             item["posiActivePower"], # Kw
             item["reverActivePower"], # Kw
@@ -429,11 +395,16 @@ class Job:
         )
 
     def run(self, backfill=False):
-        self.__create_table()
-        # today = datetime.datetime.strptime("2023-10-12", "%Y-%m-%d")
-        today = datetime.datetime.now()
-        self.get_history(today.date(), today.date())
+        self.backfill_data()
         return
+
+        self.__create_table()
+        start = datetime.datetime.strptime("2023-10-06", "%Y-%m-%d")
+        end = datetime.datetime.strptime("2023-10-10", "%Y-%m-%d")
+        # today = datetime.datetime.now()
+        self.get_history(start.date(), end.date())
+        return
+        
         
         # if backfill:
         #     self.backfill_data()
@@ -464,13 +435,17 @@ class Job:
 
         if type(time_series_data) != list:
             time_series_data = [time_series_data]
-
+        
         cursor.executemany(
             f"INSERT OR REPLACE INTO {table_name} VALUES(?, ?, ?, ?, ?, ?)", time_series_data
         )
         connection.commit()
+        logger.info(f"INSERT {table_name} {len(time_series_data)} rows")   
+        
+        cursor.close()
+        connection.close()
 
-    def _insert_plant(self, time_series_data: List[Tuple], table_name: str) -> None:
+    def _insert_plant(self, time_series_data: List[Tuple], table_name: str, plant_id: str) -> None:
         connection = sqlite3.connect(DATABASE_NAME)
         cursor = connection.cursor()
 
@@ -481,6 +456,10 @@ class Job:
             f"INSERT OR REPLACE INTO {table_name} VALUES(?, ?, ?, ?, ?)", time_series_data
         )
         connection.commit()
+        logger.info(f"INSERT {table_name} {len(time_series_data)} rows from plandId:{plant_id}")   
+        
+        cursor.close()
+        connection.close()
 
     def _insert(self, time_series_data: List[Tuple], table_name: str) -> None:
         connection = sqlite3.connect(DATABASE_NAME)
@@ -495,13 +474,19 @@ class Job:
         connection.commit()
 
     def calendar_to_timestamp(self, calendar: list):
+        
+        if 'calendar' in calendar:
+            date = calendar["calendar"]
+        elif 'time' in calendar:
+            date = calendar["time"]
+
         return int(datetime.datetime(
-            calendar["year"], 
-            calendar["month"], 
-            calendar["dayOfMonth"], 
-            calendar["hourOfDay"], 
-            calendar["minute"], 
-            calendar["second"]
+            date["year"], 
+            date["month"] + 1, 
+            date["dayOfMonth"], 
+            date["hourOfDay"], 
+            date["minute"], 
+            date["second"]
         ).timestamp())
         
 if __name__ == "__main__":
